61a580d4fd9f3b3441c13231765da885226a9f4d
dustin.liu62 <dustin.liu62@TI03BY45>
2019-01-19 03:45:04 +0800

    fixed final section

---
+++ BowlingGenius/Game.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingGenius
+{
+    public class Game
+    {
+        private int _totalScore;
+        private readonly RollingSections _rollingSections = new RollingSections();
+
+        public void Roll(int i)
+        {
+            var rollingSection = _rollingSections.GetLatestSection();
+            if (rollingSection == null || rollingSection.IsFull)
+            {
+                _rollingSections.CreateNewSection();
+            }
+            _rollingSections.Roll(i);
+        }
+
+        public int Score()
+        {
+            return _rollingSections.Score();
+        }
+    }
+
+    internal class RollingSections
+    {
+        private readonly List<RollingSection> _rollingSections = new List<RollingSection>();
+
+        public RollingSection GetLatestSection()
+        {
+            return LatestSection;
+        }
+
+        private RollingSection LatestSection => _rollingSections.LastOrDefault();
+
+        public void CreateNewSection()
+        {
+            var rollingSection = new RollingSection();
+            _rollingSections.Add(rollingSection);
+            if (IsFinalSection)
+            {
+                rollingSection.IsFinalSection = true;
+            }
+        }
+
+        private bool IsFinalSection => _rollingSections.Count == 10;
+
+        public void Roll(int point)
+        {
+            if (IsFinalSection)
+            {
+                FinalSectionRoll(point);
+            }
+            else
+            {
+                NomarlRoll(point);
+            }
+        }
+
+        private void NomarlRoll(int point)
+        {
+            LatestSection.Roll(point);
+            foreach (var rollingSection in _rollingSections.Except(EnumerableOfLatest()))
+            {
+                rollingSection.UpdateBonusScore(point);
+            }
+        }
+
+        private void FinalSectionRoll(int point)
+        {
+            LatestSection.RollEndSection(point);
+        }
+
+        private IEnumerable<RollingSection> EnumerableOfLatest()
+        {
+            yield return LatestSection;
+        }
+
+        public int Score()
+        {
+            return _rollingSections.Sum(x => x.Score());
+        }
+    }
+
+    public class RollingSection
+    {
+        private int _score;
+        public Bonus Bonus = new Bonus();
+        private int _ball;
+        public bool IsFull => !IsFinalSection && (_ball == 2 || Bonus.HasBonus);
+        public bool IsFinalSection { get; set; }
+
+        public void Roll(int point)
+        {
+            _score = _score + point;
+            _ball++;
+            if (_score == 10)
+            {
+                if (_ball == 2)
+                {
+                    Bonus.Spare();
+                }
+                else
+                {
+                    Bonus.Strike();
+                }
+            }
+        }
+
+        public int Score()
+        {
+            return IsEnd() ? _score : 0;
+        }
+
+        private bool IsEnd()
+        {
+            return (_ball >= 2 && !Bonus.HasBonus) || (Bonus.HasBonus && Bonus.Times == 0);
+        }
+
+        public void UpdateBonusScore(int point)
+        {
+            if (Bonus.Times > 0)
+            {
+                Bonus.Times--;
+                _score = _score + point;
+            }
+        }
+
+        public void RollEndSection(int point)
+        {
+            if ((_score == 10 && _ball <= 3) || _ball <= 2)
+            {
+                _score = _score + point;
+            }
+            _ball++;
+        }
+    }
+
+    public class Bonus
+    {
+        public int Times;
+
+        public void Spare()
+        {
+            HasBonus = true;
+            Times = 1;
+        }
+
+        public bool HasBonus { get; set; }
+
+        public void Strike()
+        {
+            HasBonus = true;
+            Times = 2;
+        }
+    }
+}
--- BowlingGenius/UnitTest1.cs
+++ BowlingGenius/UnitTest1.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BowlingGenius
 {
@@ -9,6 +8,239 @@ namespace BowlingGenius
         [TestMethod]
         public void TestMethod1()
         {
+            var game = new Game();
+            game.Roll(1);
+            int score = game.Score();
+            Assert.AreEqual(0, score);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(1);
+            int score = game.Score();
+            Assert.AreEqual(2, score);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(9);
+            int score = game.Score();
+            Assert.AreEqual(0, score);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(9);
+            game.Roll(1);
+            int score = game.Score();
+            Assert.AreEqual(11, score);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(9);
+            game.Roll(1);
+            game.Roll(3);
+            int score = game.Score();
+            Assert.AreEqual(15, score);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(9);
+            game.Roll(1);
+            game.Roll(3);
+            game.Roll(7);
+            int score = game.Score();
+            Assert.AreEqual(15, score);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(9);
+            game.Roll(7);
+            game.Roll(3);
+            int score = game.Score();
+            Assert.AreEqual(17, score);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(9);
+            game.Roll(7);
+            game.Roll(3);
+            game.Roll(8);
+            int score = game.Score();
+            Assert.AreEqual(35, score);
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(5);
+            int score = game.Score();
+            Assert.AreEqual(26, score);
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(3);
+            int score = game.Score();
+            Assert.AreEqual(23, score);
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(3);
+            int score = game.Score();
+            Assert.AreEqual(45, score);
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            var game = new Game();
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(0);
+            int score = game.Score();
+            Assert.AreEqual(39, score);
+        }
+
+        [TestMethod]
+        public void TestMethod13()
+        {
+            var game = new Game();
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(3);
+            game.Roll(0);
+            int score = game.Score();
+            Assert.AreEqual(3, score);
+        }
+
+        [TestMethod]
+        public void TestMethod14()
+        {
+            var game = new Game();
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            int score = game.Score();
+            Assert.AreEqual(30, score);
+        }
+
+        [TestMethod]
+        public void TestMethod15()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(3);
+            game.Roll(7);
+            game.Roll(10);
+            int score = game.Score();
+            Assert.AreEqual(21, score);
+        }
+
+        [TestMethod]
+        public void TestMethod16()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(0);
+            game.Roll(3);
+            game.Roll(7);
+            game.Roll(10);
+            game.Roll(1);
+            game.Roll(2);
+            int score = game.Score();
+            Assert.AreEqual(21, score);
         }
     }
 }
